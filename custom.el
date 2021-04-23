2837a65c3b93a44741981a8065a21aad769575d5
PlanetsFuture <danny.ramrez7@gmail.com>
2021-04-23 21:10:53 +0200

    custom.el

---
+++ custom.el
@@ -0,0 +1,48 @@
+(setq danny-full-name "Danny Ramírez")
+(setq danny-mail-address "danny.ramrez7@gmail.com")
+(setq danny-theme 'classic)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(anki-editor-create-decks t)
+ '(custom-safe-themes
+   '("990e24b406787568c592db2b853aa65ecc2dcd08146c0d22293259d400174e37" default))
+ '(danny-package-archives 'melpa)
+ '(danny-theme 'warm))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit font-lock-keyword-face :bold t :height 3.0))))
+ '(aw-minibuffer-leading-char-face ((t (:inherit font-lock-keyword-face :bold t :height 2.0))))
+ '(aw-mode-line-face ((t (:inherit mode-line-emphasis :bold t))))
+ '(dashboard-heading ((t (:inherit (font-lock-string-face bold)))))
+ '(diff-hl-change ((t (:foreground "#268bd2" :background nil))))
+ '(diff-hl-delete ((t (:background nil))))
+ '(diff-hl-insert ((t (:background nil))))
+ '(doom-modeline-buffer-file ((t (:inherit (mode-line bold)))))
+ '(git-timemachine-minibuffer-author-face ((t (:inherit success))))
+ '(git-timemachine-minibuffer-detail-face ((t (:inherit warning))))
+ '(ivy-minibuffer-match-face-1 ((t (:inherit font-lock-doc-face :foreground nil))))
+ '(org-ellipsis ((t (:foreground nil))))
+ '(org-pomodoro-mode-line ((t (:inherit warning))))
+ '(org-pomodoro-mode-line-break ((t (:inherit success))))
+ '(org-pomodoro-mode-line-overtime ((t (:inherit error))))
+ '(pulse-highlight-face ((t (:inherit region))))
+ '(pulse-highlight-start-face ((t (:inherit region))))
+ '(symbol-overlay-default-face ((t (:inherit (region bold)))))
+ '(ztreep-arrow-face ((t (:inherit font-lock-comment-face))))
+ '(ztreep-diff-header-face ((t (:inherit (diff-header bold)))))
+ '(ztreep-diff-header-small-face ((t (:inherit diff-file-header))))
+ '(ztreep-diff-model-add-face ((t (:inherit diff-nonexistent))))
+ '(ztreep-diff-model-diff-face ((t (:inherit diff-removed))))
+ '(ztreep-diff-model-ignored-face ((t (:inherit font-lock-doc-face :strike-through t))))
+ '(ztreep-diff-model-normal-face ((t (:inherit font-lock-doc-face))))
+ '(ztreep-expand-sign-face ((t (:inherit font-lock-function-name-face))))
+ '(ztreep-header-face ((t (:inherit diff-header))))
+ '(ztreep-leaf-face ((t (:inherit diff-index))))
+ '(ztreep-node-face ((t (:inherit font-lock-variable-name-face)))))
