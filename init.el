ceb32895168f627a49047089dbb14574b8826343
Danny Ramírez <danny.ramrez7@gmail.com>
2021-06-12 19:52:03 +0200

    change gc-conss-threshold und gc-cons-upper-limit

--- init.el
+++ init.el
@@ -11,11 +11,11 @@
   (error "This requires Emacs 25.1 and above!"))
 
 ;; Speed up startup
-(defvar danny-gc-cons-threshold (if (display-graphic-p) 16000000 1600000)
+(defvar danny-gc-cons-threshold (if (display-graphic-p) 64000000 1600000)
   "The default value to use for `gc-cons-threshold'. If you experience freezing,
 decrease this. If you experience stuttering, increase this.")
 
-(defvar danny-gc-cons-upper-limit (if (display-graphic-p) 400000000 100000000)
+(defvar danny-gc-cons-upper-limit (if (display-graphic-p) 512000000 128000000)
   "The temporary value for `gc-cons-threshold' to defer it.")
 
 (defvar danny-gc-timer (run-with-idle-timer 10 t #'garbage-collect)
